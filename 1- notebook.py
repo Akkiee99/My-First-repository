a15fe777d2943ad447a08b228bb480401c3fe420
Akkiee99 <130033019+Akkiee99@users.noreply.github.com>
2023-04-08 10:58:55 +0400

    Add files via upload

---
+++ 1- notebook.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ### Objectives:
+# - List popular languages for Data Science
+# - List Some popular libraries used by Data Scientist
+# - Explore Data Science Tools
+# - Explore arithmetic expression in Python
+
+# Some of the popular language that Data Scientists use are: 
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Julia
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. Pandas
+# 2. Numpy
+# 3. Matplotlib
+# 4. Seaborn
+# 5. Pytorch
+
+# ## Data Science Tools
+# 
+# 1. Jupiter Notbook
+# 2. RStudio
+# 3. Apache Zeppelin
+# 
+
+# ### Below are a few examples few evaluating arithmetic expressions in Python
+
+# In[4]:
+
+
+# This is a simpke arthmetic expression to multiply then add integers
+
+(3*4)+5
+
+
+# In[8]:
+
+
+# This will convert 200 minutes in hous by dividing by 60
+
+minutes = 200
+hours = minutes / 60
+print("Hours: ", hours)
+
+
+# ## Author
+# Akshay kumar
+
+# In[ ]:
+
+
+
+
